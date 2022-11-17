c3263029de2a98a2db3d0c2f93132743c1a66bf0
Holoc Sergiu <vubafo@gmail.com>
2022-11-18 00:58:51 +0200

    am adaugat tema

---
+++ C5/script.js
@@ -0,0 +1,44 @@
+var myHobbies = ['gym', 'walking', 'playing', 'reading', 'swimming', 'observing'];
+
+yourHobbies = myHobbies; 
+
+myHobbies = ["Hobby-ul meu preferat este reading"];
+
+var myPerson = {
+  firstName: "Sergiu", 
+  lastName: "Holoc",
+  age: 24,
+  number: 084358743,
+  gender: "Male",
+  hobby1: "gym",
+  hobby2:  "walking",
+  hobby3: "playing",
+  hobby4: "reading",
+  hobby5: "swimming",
+  hobby6: "observing", 
+}
+
+var firstName = "Sergiu";
+var myfirstName = firstName;
+
+var secondName = "Ionut";
+var mysecondName = secondName;
+
+var lastName = "Holoc";
+var mylastName = lastName;
+
+var age = "24";
+var myAge = age;
+
+var number = "0755555533";
+var myNumber = number; 
+
+var gender = "male"; 
+var myGender = gender; 
+
+function sayMyDetails(){
+  return `My name is Sergiu-Ionut Holoc and I am 24 yo, my phone number is 0755555533, 
+  I identify myself as a male and my favorite hobbies are: reading, walking, swimming ` ;
+}
+
+
