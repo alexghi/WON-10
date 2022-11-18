a26f75a4ce48b4ba5f7d02e1dff251dc4dc67f32
Eliza Rotaru <wheelerokay@gmail.com>
2022-11-18 16:03:53 +0200

    tema partial

--- curs_05_Javascript_Introduction/script.js
+++ curs_05_Javascript_Introduction/script.js
@@ -1,24 +1,19 @@
-var myName = "Alex";
-var yourName = myName;
+function hobby(NumeHobby){
+    var NumeHobby=["gaming","video editing","filming","drawing"];
+     return `Hobby-urile mele sunt ${NumeHobby}.`;
+ };
 
-var age = 20;
-var height = 1.92;
-var isMale = true;
+ var myDetails={
+     nume:"Rotaru",
+     prenume: "Eliza",
+     varsta:20,
+     telefon: "0745332208",
+     gen: "feminin",
+ };
 
-//   var myAddress = {
-//     city: "Oradea",
-//     country: "RO",
-//     age: 20,
-//   };
-var hobbies = ["reading", "tech", "biking"];
-var sayHello = function (a) {
-  return "hello";
-};
-
-function sayMyDetails(yourName, yourAge) {
-  return `numele tau este ${yourName}, iar varsta ta este ${yourAge}`;
-}
-
-var sayAge = (someAge) => {
-  return someAge;
+ function detalii(myName,myAge,NumeHobby){
+     var NumeHobby=["gaming","video editing","filming","drawing"];
+     var myAge= 20;
+     var myName="Liz";
+    return `Numele meu este ${myName}, am ${myAge} ani si printre hobby-urile mele se afla:${NumeHobby}.`;
 };
