31cd4ee8c55dc864e68acaa2d7047ff0a5954825
Bunea Rafael <bunea.haiducu@gmail.com>
2022-11-24 22:46:01 +0200

    created js form validation

--- Assignments/Curs-06 JS exercitii/app.js
+++ Assignments/Curs-06 JS exercitii/app.js
@@ -2,6 +2,7 @@
 
 var number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
 var prime=[];
+
 function primeNumbers(num) {
     if(num<2) {
     return false
@@ -41,3 +42,32 @@ function vowelDetector(str) {
 }
 
 console.log(vowelDetector('How many vowels are in this sentence?'));
+
+// Form Validation
+
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const age = document.getElementById("age");
+const errorElement = document.getElementById('error');
+
+form.addEventListener("submit", (e)=> {
+    let messages=[];
+    if(username.value.length <= 3) {
+        messages.push("Name must be longer than 3")
+    }
+    if(password.value.length <= 1) {
+        messages.push('Password must be longer than 1')
+    }
+    if(age.value <= 16) {
+        messages.push('Age must be over 16')
+    }
+    if(email.value.indexOf(".ro") == -1) {
+        messages.push('Email must have a Romanian domain\nExample:\'yourname@domain.ro\'')
+    }
+    if(messages.length > 0 ) {
+    e.preventDefault();
+    errorElement.innerHTML = messages.join(", ")
+}
+
+})
