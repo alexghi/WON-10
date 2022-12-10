05dea933633e3ba8ff0f07b38cef931acd19b7aa
Borze Andrei <borzeandrew@gmail.com>
2022-12-10 20:23:00 +0200

    Merge with c8

---
+++ curs_08_Javascript_Modules/common/arithmetic.js
@@ -0,0 +1,15 @@
+export const add = (a, b) => a + b
+
+export const add2 = function (a, b ){
+    return a + b;
+}
+
+export const devide = function (a, b){
+    return a/b;
+}
+
+export const multiply = function (a, b){
+    return a*b;
+}
+
+export default multiply;
---
+++ curs_08_Javascript_Modules/index.js
@@ -0,0 +1,9 @@
+
+import myFancyAdd, { add,  devide } from "./common/arithmetic";
+
+import myFancyMultiply from "./common/arithmetic"
+
+const somthing = "Rezultatul adunarii este:  " + add(12, 12);
+const somthing2 = "Rezultatul scaderii este:  "  + devide(24,6);
+console.log(somthing);
+console.log(somthing2);
